01e8b5479a9c2d075b4288ff1e993b31184a2c4c
LittleBlackBird <122574716+LittleBlackBird@users.noreply.github.com>
2026-01-10 08:17:28 -0800

    Update practice_reward.js

--- js/practice_reward.js
+++ js/practice_reward.js
@@ -1,9 +1,9 @@
 // ===== Reward videos =====
 const rewardVideos = [
-  new URL("assets/videos/Dragon3.mp4", document.baseURI).href,
-  new URL("assets/videos/Dragon4.mp4", document.baseURI).href,
-  new URL("assets/videos/Dragon5.mp4", document.baseURI).href,
-  new URL("assets/videos/Dragon1.mp4", document.baseURI).href,
+  // new URL("assets/videos/Dragon3.mp4", document.baseURI).href,
+  // new URL("assets/videos/Dragon4.mp4", document.baseURI).href,
+  // new URL("assets/videos/Dragon5.mp4", document.baseURI).href,
+  // new URL("assets/videos/Dragon1.mp4", document.baseURI).href,
   new URL("assets/videos/Cereal1.mp4", document.baseURI).href,
 ];
 
